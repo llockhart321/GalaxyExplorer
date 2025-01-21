8c3ff84476378fccd3149379461329395e002ef4
Luke Lockhart <llockhar0022@pointloma.edu>
2025-01-21 14:50:38 -0800

    star background

--- AnimationHandler.java
+++ AnimationHandler.java
@@ -4,12 +4,19 @@ import javafx.scene.paint.*;
 
 public class AnimationHandler extends AnimationTimer {
    private GraphicsContext gc;
+   private Canvas canvas;
    private int x = 0;
-   public AnimationHandler(GraphicsContext gc) { this.gc = gc; }
+   private StarSystem system;
+   
+   public AnimationHandler(GraphicsContext gc) { 
+      this.gc = gc; 
+      system = new StarSystem();
+   }
    public void handle(long currentTimeInNanoSeconds) {
       gc.clearRect(0, 0, gc.getCanvas().getWidth(), gc.getCanvas().getHeight());
       gc.setFill(Color.WHITE);
       gc.fillRect(x, 50, 50, 50);
       x++;
+      system.drawMe(gc);
    }
 }
--- StarSystem.java
+++ StarSystem.java
@@ -1,10 +1,23 @@
-import javafx.scene.paint.*;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.*;
+
 public class StarSystem {
    private List <Planet> planets;
    private List <Gate> gates;
+   private ArrayList<Double> starX;
+   private ArrayList<Double> starY;
+   private ArrayList<Double> starRadius;
 
    public StarSystem() {
+      starX = new ArrayList<Double>();
+      starY = new ArrayList<Double>();
+      starRadius = new ArrayList<Double>();
+      
       Random rand = new Random();
       int numPlanets = rand.nextInt(5) + 1;  // Random number between 1 and 5
       // Set number of gates from an outside input
@@ -16,6 +29,34 @@ public class StarSystem {
          planets.add(new Planet(Color.BLUE, 0, 0, 0, 0));
       }
       
+        int numStars = 100; // Number of stars to draw
+        for (int i = 0; i < numStars; i++) {
+            double x = rand.nextDouble() * 1000;
+            double y = rand.nextDouble() * 1000;
+            double radius = rand.nextDouble() * 2 + 1; // Random size for stars (1-3 pixels)
+            
+            starX.add(x);
+            starY.add(y);
+            starRadius.add(radius);
+        }
+      
       // Fill the gates arraylist
    }
+   
+
+   
+   public void drawMe(GraphicsContext gc) {
+        // Set the background to black
+        gc.setFill(Color.BLACK);
+        gc.fillRect(0, 0, 1000,1000);
+        gc.setFill(Color.WHITE);
+
+        for(int i=0; i<starX.size(); i++)
+        {
+            gc.fillOval(starX.get(i), starY.get(i), starRadius.get(i), starRadius.get(i));
+        }
+        
+        // Optionally: Draw planets or gates (if implemented)
+        // This example focuses on the starry background.
+    }   
 }
