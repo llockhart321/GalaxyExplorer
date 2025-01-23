db817d8532779529a9f0da8b306cb41c89d520bd
Mitch <mwoolley0021@pointloma.edu>
2025-01-23 12:48:29 -0800

    More base code before the build

--- AnimationHandler.java
+++ AnimationHandler.java
@@ -7,15 +7,17 @@ public class AnimationHandler extends AnimationTimer {
    private Canvas canvas;
    private int x = 0;
    private StarSystem system;
+   private Camera c;
    
    public AnimationHandler(GraphicsContext gc) { 
-      this.gc = gc; 
+      this.gc = gc;
+      c = new Camera();
      
    }
    public void handle(long currentTimeInNanoSeconds) {
       system =  Player.getInstance().getSystem();
       gc.clearRect(0, 0, gc.getCanvas().getWidth(), gc.getCanvas().getHeight());
-      system.drawMe(gc, 0, 0);
+      system.drawMe(gc, c.getCameraOffsetX(), c.getCameraOffsetY());
       system.collisionCheck(gc);
       Player.getInstance().drawMe(gc);
       PlayerMovementState.getInstance().move();
